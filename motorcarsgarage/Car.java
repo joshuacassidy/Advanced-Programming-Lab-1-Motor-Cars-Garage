b354b097641bbe5cc50afd55504e8596336d5e0d
joshuacassidy <Joshcassidy79@gmail.com>
2018-09-22 10:14:47 +0100

    Initial commit

---
+++ motorcarsgarage/Car.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package motorcarsgarage;
+
+/**
+ *
+ * @author cmuntean
+ */
+public class Car {
+public String model; 
+public String color;   
+public int year;
+
+ public Car (String inModel, String inColor, int inYear)
+    {
+        // write your code here
+    }
+
+public String getModel()   
+{
+    // write your code here
+    return "";
+}
+public void setModel(String inModel)
+{
+    // write your code here
+}
+public String getColor()   
+{
+    // write your code here
+    return "";
+}
+public void setColor(String inColor)
+{
+    // write your code here
+}
+public int getYear()
+{
+    // write your code here
+    return -1;
+}
+public void setYear(int inYear)  
+{
+    // write your code here
+}
+public void printCar()
+{
+    // write your code here
+}
+
+    
+}
---
+++ motorcarsgarage/MotorCarsGarage.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package motorcarsgarage;
+
+/**
+ *
+ * @author cmuntean
+ */
+public class MotorCarsGarage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("MotorCarsGarage");
+        System.out.println("10, Drumcondra Road, Dubin 5");
+        System.out.println("Phone: 563400");
+        
+        // write your code here
+	   // create one object Car type
+	   //set the model, color and year of the Car object
+	   // print the details of the Car objecr
+
+    }
+}
